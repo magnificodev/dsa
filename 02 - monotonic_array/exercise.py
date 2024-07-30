95ff1ed1f08edc9272a2345779fc55e3ee9be7de
magnificodev <magnificodev8@gmail.com>
2024-07-31 00:29:21 +0700

    02 - Monotonic Array - Method 2 Updated

--- 02 - monotonic_array/exercise.py
+++ 02 - monotonic_array/exercise.py
@@ -1,21 +1,46 @@
-def check_monitoric_increasing_arr(array, n):
-    for i in range(n - 1):
-        if array[i] - array[i + 1] > 0:
-            return False
-    return True
+# METHOD 1: MY SOLUTION
+# def check_monitoric_increasing_arr(array, n):
+#     for i in range(n - 1):
+#         if array[i] - array[i + 1] > 0:
+#             return False
+#     return True
 
 
-def check_monitoric_decreasing_arr(array, n):
-    for i in range(n - 1):
-        if array[i] - array[i + 1] < 0:
-            return False
-    return True
+# def check_monitoric_decreasing_arr(array, n):
+#     for i in range(n - 1):
+#         if array[i] - array[i + 1] < 0:
+#             return False
+#     return True
 
 
+# def monotonic_array(array):
+#     n = len(array)
+#     if n <= 1:
+#         return True
+#     return check_monitoric_increasing_arr(array, n) or check_monitoric_decreasing_arr(
+#         array, n
+#     )
+
+
+# METHOD 2
 def monotonic_array(array):
     n = len(array)
-    if n <= 1:
+    if n < 2:
         return True
-    return check_monitoric_increasing_arr(array, n) or check_monitoric_decreasing_arr(
-        array, n
-    )
+
+    first = array[0]
+    last = array[n - 1]
+
+    if first > last:
+        for i in range(n - 1):
+            if array[i] < array[i + 1]:
+                return False
+    elif first < last:
+        for i in range(n - 1):
+            if array[i] > array[i + 1]:
+                return False
+    else:
+        for i in range(n - 1):
+            if array[i] != first:
+                return False
+    return True
